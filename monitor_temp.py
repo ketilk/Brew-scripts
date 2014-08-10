cddbbd3e87ee39f9781ec1933fa77a2761a20603
Ketil Karstad <ketil.karstad@gmail.com>
2014-08-10 15:17:07 +0200

    Updated all scripts to handle interupt signals. Xively script closer to completion.

--- monitor_temp.py
+++ monitor_temp.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python
 
+import signal
+import sys
+
 from Interfaces.ds18b20 import DS18B20
 from Atlas.atlas import Atlas
 from Atlas.topic import *
@@ -18,17 +21,17 @@ def main():
   with Atlas() as atlas:
     sensor = DS18B20('28-000004f10b89')
     temp = sensor.get_temperature()
-    topic = Topic(TopicDescription('temperature', 'ferm1.sensor1'), temp)
+    topic = Topic(TopicDescription('temperature', 'ferm1_sensor1'), temp)
     publisher = atlas.get_publisher(topic)
     sensor_publisher_tuplets.append((sensor, publisher))
     sensor = DS18B20('28-000004f1f9bf')
     temp = sensor.get_temperature()
-    topic = Topic(TopicDescription('temperature', 'ferm1.sensor2'), temp)
+    topic = Topic(TopicDescription('temperature', 'ferm1_sensor2'), temp)
     publisher = atlas.get_publisher(topic)
     sensor_publisher_tuplets.append((sensor, publisher))
     sensor = DS18B20('28-00000522683f')
     temp = sensor.get_temperature()
-    topic = Topic(TopicDescription('temperature', 'ferm1.sensor3'), temp)
+    topic = Topic(TopicDescription('temperature', 'ferm1_sensor3'), temp)
     publisher = atlas.get_publisher(topic)
     sensor_publisher_tuplets.append((sensor, publisher))
     
@@ -36,6 +39,10 @@ def main():
       for tuplet in sensor_publisher_tuplets:
         tuplet[1].publish(tuplet[0].get_temperature())
       time.sleep(1)
+
+def signal_handler(signal, frame):
+  sys.exit(0)
   
 if __name__ == '__main__':
+  signal.signal(signal.SIGINT, signal_handler)
   main()
--- xively_logger.py
+++ xively_logger.py
@@ -1,23 +1,82 @@
 #!/usr/bin/python
 
+import signal
+import sys
+
 from Atlas.atlas import *
 from Atlas.topic import *
 import xively
+import time
+import datetime
+import requests
 
 import logging
 
 FEED_ID = '1620077696'
 API_KEY = '71cG08OmuBBUJb13HdzO2sDCCE4zCDelFxYgPNvYm0IePRox'
 
+xively_api = xively.XivelyAPIClient(API_KEY)
+
+def get_subscriber_stream_tuplet(atlas, feed, topic_name, key, logger):
+  topic = TopicDescription(topic_name, key)
+  loop = True
+  while loop:
+    try:
+      subscriber = atlas.get_subscriber(topic)
+    except AtlasError:
+      logger.warning('Could not get subscriber on topic: '
+                      + str(topic) + '. Sleeping 3 seconds.')
+      time.sleep(3)
+    else:
+      logger.info('Subscriber up on topic ' + str(topic))
+      loop = False
+  loop = True
+  while loop:
+    try:
+      data_stream = feed.datastreams.get(key)
+    except:
+      logger.warning('Could not get data stream on key: '
+                      + key + '. Sleeping 3 seconds')
+      time.sleep(3)
+    else:
+      logger.info('Data stream established on key: ' + key + '.')
+      loop = False
+  return (subscriber, data_stream)
+    
 def main():
   logging.basicConfig(filename='xively_logger.log',
     filemode='a',
     format='%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s',
     datefmt='%H:%M:%S',
     level=logging.DEBUG)
+  logger = logging.getLogger(__name__)
     
-    subscriber_stream_tuplets = []
+  subscriber_stream_tuplets = []
+  feed = xively_api.feeds.get('FEED_ID')
+  
+  with Atlas() as atlas:
+    subscriber_stream_tuplets.append(get_subscriber_stream_tuplet(
+      atlas, feed, 'temperature', 'ferm1_sensor1', logger
+    ))
+    subscriber_stream_tuplets.append(get_subscriber_stream_tuplet(
+      atlas, feed, 'temperature', 'ferm1_sensor2', logger
+    ))
+    subscriber_stream_tuplets.append(get_subscriber_stream_tuplet(
+      atlas, feed, 'temperature', 'ferm1_sensor3', logger
+    ))
+    while True:
+      for tuplet in subscriber_stream_tuplets:
+        tuplet[1].current_value = tuplet[0].topic.payload
+        tuplet[1].at = datetime.datetime.utcnow()
+        try:
+          tuplet[1].update()
+        except requests.HTTPError as e:
+          logger.warning('HTTPError({0}): {1}'.format(e.errno, e.strerror)
+      time.sleep(5)
     
-    with Atlas() as atlas:
-      topic = TopicDescription('temperature', 'ferm1.sensor1')
-      subscriber = atlas.get_subscriber
+def signal_handler(signal, frame):
+  sys.exit(0)
+  
+if __name__ == '__main__':
+  signal.signal(signal.SIGINT, signal_handler)
+  main()
