13d3fe18b438ce0e226e0a311d495205de2f87bb
Ketil Karstad <ketil.karstad@gmail.com>
2014-08-11 20:59:15 +0200

    Changed scripts into daemons.

---
+++ temperature_monitor.py
@@ -0,0 +1,66 @@
+#!/usr/bin/python
+
+import signal
+import sys
+import os
+
+from daemon import Daemon
+from Interfaces.ds18b20 import DS18B20
+from Atlas.atlas import Atlas
+from Atlas.topic import *
+
+import logging
+
+file_name = os.path.basename(__file__)
+
+class TemperatureMonitorDaemon(Daemon):
+  def run(self):
+    logging.basicConfig(filename=file_name + '.log',
+      filemode='a',
+      format='%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s',
+      datefmt='%H:%M:%S',
+      level=logging.DEBUG)
+    logger = logging.getLogger(__name__)
+  
+    sensor_publisher_tuplets = []
+  
+    with Atlas() as atlas:
+      sensor = DS18B20('28-000004f10b89')
+      temp = sensor.get_temperature()
+      topic = Topic(TopicDescription('temperature', 'ferm1_sensor1'), temp)
+      publisher = atlas.get_publisher(topic)
+      sensor_publisher_tuplets.append((sensor, publisher))
+      sensor = DS18B20('28-000004f1f9bf')
+      temp = sensor.get_temperature()
+      topic = Topic(TopicDescription('temperature', 'ferm1_sensor2'), temp)
+      publisher = atlas.get_publisher(topic)
+      sensor_publisher_tuplets.append((sensor, publisher))
+      sensor = DS18B20('28-00000522683f')
+      temp = sensor.get_temperature()
+      topic = Topic(TopicDescription('temperature', 'ferm1_sensor3'), temp)
+      publisher = atlas.get_publisher(topic)
+      sensor_publisher_tuplets.append((sensor, publisher))
+    
+      logger.info('Objects instantiated.')
+    
+      while True:
+        for tuplet in sensor_publisher_tuplets:
+          tuplet[1].publish(tuplet[0].get_temperature())
+        time.sleep(1)
+  
+if __name__ == '__main__':
+  daemon = TemperatureMonitorDaemon('/tmp/temperature_monitor.pid')
+  if len(sys.argv) == 2:
+    if 'start' == sys.argv[1]:
+      daemon.start()
+    elif 'stop' == sys.argv[1]:
+      daemon.stop()
+    elif 'restart' == sys.argv[1]:
+      daemon.restart()
+    else:
+      print "Unknown command"
+      sys.exit(2)
+    sys.exit(0)
+  else:
+    print "usage: %s start|stop|restart" % sys.argv[0]
+    sys.exit(2)
--- xively_logger.py
+++ xively_logger.py
@@ -4,6 +4,7 @@ import signal
 import sys
 import os
 
+from daemon import Daemon
 from Atlas.atlas import *
 from Atlas.topic import *
 import xively
@@ -19,6 +20,38 @@ file_name = os.path.basename(__file__)
 
 xively_api = xively.XivelyAPIClient(API_KEY)
 
+class XivelyLoggerDaemon(Daemon):
+  def run(self):
+    logging.basicConfig(filename=file_name + '.log',
+      filemode='a',
+      format='%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s',
+      datefmt='%H:%M:%S',
+      level=logging.DEBUG)
+    logger = logging.getLogger(__name__)
+    
+    subscriber_stream_tuplets = []
+    feed = xively_api.feeds.get(FEED_ID)
+  
+    with Atlas() as atlas:
+      subscriber_stream_tuplets.append(get_subscriber_stream_tuplet(
+        atlas, feed, 'temperature', 'ferm1_sensor1', logger
+      ))
+      subscriber_stream_tuplets.append(get_subscriber_stream_tuplet(
+        atlas, feed, 'temperature', 'ferm1_sensor2', logger
+      ))
+      subscriber_stream_tuplets.append(get_subscriber_stream_tuplet(
+        atlas, feed, 'temperature', 'ferm1_sensor3', logger
+      ))
+      while True:
+        for tuplet in subscriber_stream_tuplets:
+          tuplet[1].current_value = tuplet[0].topic.payload
+          tuplet[1].at = datetime.datetime.utcnow()
+          try:
+            tuplet[1].update()
+          except requests.HTTPError as e:
+            logger.warning('HTTPError({0}): {1}'.format(e.errno, e.strerror))
+        time.sleep(5)
+        
 def get_subscriber_stream_tuplet(atlas, feed, topic_name, key, logger):
   topic = TopicDescription(topic_name, key)
   loop = True
@@ -44,41 +77,20 @@ def get_subscriber_stream_tuplet(atlas, feed, topic_name, key, logger):
       logger.info('Data stream established on key: ' + key + '.')
       loop = False
   return (subscriber, data_stream)
-    
-def main():
-  logging.basicConfig(filename=file_name + '.log',
-    filemode='a',
-    format='%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s',
-    datefmt='%H:%M:%S',
-    level=logging.DEBUG)
-  logger = logging.getLogger(__name__)
-    
-  subscriber_stream_tuplets = []
-  feed = xively_api.feeds.get(FEED_ID)
-  
-  with Atlas() as atlas:
-    subscriber_stream_tuplets.append(get_subscriber_stream_tuplet(
-      atlas, feed, 'temperature', 'ferm1_sensor1', logger
-    ))
-    subscriber_stream_tuplets.append(get_subscriber_stream_tuplet(
-      atlas, feed, 'temperature', 'ferm1_sensor2', logger
-    ))
-    subscriber_stream_tuplets.append(get_subscriber_stream_tuplet(
-      atlas, feed, 'temperature', 'ferm1_sensor3', logger
-    ))
-    while True:
-      for tuplet in subscriber_stream_tuplets:
-        tuplet[1].current_value = tuplet[0].topic.payload
-        tuplet[1].at = datetime.datetime.utcnow()
-        try:
-          tuplet[1].update()
-        except requests.HTTPError as e:
-          logger.warning('HTTPError({0}): {1}'.format(e.errno, e.strerror))
-      time.sleep(5)
-    
-def signal_handler(signal, frame):
-  sys.exit(0)
   
 if __name__ == '__main__':
-  signal.signal(signal.SIGINT, signal_handler)
-  main()
+  daemon = XivelyLoggerDaemon('/tmp/xively_logger.pid')
+  if len(sys.argv) == 2:
+    if 'start' == sys.argv[1]:
+      daemon.start()
+    elif 'stop' == sys.argv[1]:
+      daemon.stop()
+    elif 'restart' == sys.argv[1]:
+      daemon.restart()
+    else:
+      print "Unknown command"
+      sys.exit(2)
+    sys.exit(0)
+  else:
+    print "usage: %s start|stop|restart" % sys.argv[0]
+    sys.exit(2)
