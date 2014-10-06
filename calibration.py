6df4ef3011b22d35b0b6940d9d5ae1363b394a55
Ketil Karstad <ketil.karstad@gmail.com>
2014-10-06 21:14:58 +0200

    calibration daemon update

--- calibration.py
+++ calibration.py
@@ -18,6 +18,9 @@ class CalibrationDaemon(AtlasDaemon):
     self.stage1 = 6 * 60 * 60 #6 hours
     self.stage2 = 6 * 60 * 60 #6 hours
     self.stage3 = 6 * 60 * 60 #6 hours
+    self.t_cycle = self.t0
+    self.cycle_period = 5 * 60 #5 minutes
+    self.power = 0.2 #20% of full power
     self.triplets = []
     
     subscriber = self.get_subscriber(Topic("temperature", "sensor1"))
@@ -37,11 +40,34 @@ class CalibrationDaemon(AtlasDaemon):
     topic = Topic("temperature", "sensor3_average", average_temp.get_value())
     publisher = self.get_publisher(topic)
     self.triplets.append((subscriber, average_temp, publisher))
+    
+    topic = Topic("state", "heater", self.pin.get_state())
+    self.heater_publisher = self.get_publisher(topic)
+    
+    topic = Topic("stage", "calibration_stage", 1)
+    self.stage_publisher = self.get_publisher(topic)
 
   def _loop(self):
+    if time.time() < self.t0 + self.stage1:
+      if self.pin.get_state():
+        self.pin.set_low()
+      self.stage_publisher.publish(1)
+    elif time.time() < self.t.0 + self.stage1 + self.stage2:
+      self.stage_publisher.publish(2)
+      if self.t_cycle + self.cycle_period < time.time():
+        self.t_cycle = time.time()
+        self.pin.set_high()
+      elif self.t_cycle + self.power * self.cycle < time.time():
+        self.pin.set_low()
+    elif self.t.0 + self.stage1 + self.stage2 < time.time():
+      if self.pin.get_state():
+        self.pin.set_low()
+      self.stage_publisher.publish(3)
+    
     for triple in self.triplets:
       triple[1].update(triple[0].topic.data)
       triple[2].publish(triple[1].get_value())
+    self.heater_publisher.publish(self.pin.get_state())
     time.sleep(1)
 
 if __name__ == '__main__':
